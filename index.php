ffd70180f61cd12913cff1e2bc529bab739dd4d4
Олег <velmisoff123@gmail.com>
2013-04-12 14:25:17 +0400

    первый коммит

---
+++ index.php
@@ -0,0 +1,73 @@
+﻿<?php 
+require_once("lumi.php");
+
+$data = lumi::get_data();  
+ //echo $data['temp_p']; echo $data['temp_r'];
+
+$temp_p = $data['temp_p'];
+$wind_p = $data['wind_p'];
+$sky_p = $data['sky_p'];
+$snow_p = $data['snow_p'];
+
+$temp_r = $data['temp_r'];
+$wind_r = $data['wind_r'];
+$sky_r = $data['sky_r'];
+$snow_r = $data['snow_r'];
+
+
+if (isset($_GET["cam"])) {
+$v22 = $_GET["cam"];
+}
+
+switch($v22) 
+			{
+				case 'Plato':
+				$v22 = 'Плато';
+				$sbody = "
+				<link url='http://172.17.6.97:8080/stream.flv' tit='Плато' temp='$temp_p' wind='$wind_p' tr='$temp_r' wr='$wind_r' snow='$snow_p' img='$sky_p' />           
+			    <link url='http://172.17.6.97:8080/stream.flv' tit='Ресторан Зима' temp='$temp_p' wind='$wind_p' snow='$snow_p' img='$sky_p' />			 					
+				<link url='http://172.17.6.97:8080/stream.flv' tit='Роза 1600' temp='$temp_r' wind='$wind_r' snow='$snow_r' img='$sky_r' />				
+				 ";
+				
+				break;
+				case 'Zima':
+				$v22 = 'Ресторан Зима';
+				$sbody = "
+				<link url='http://172.17.6.97:8080/stream.flv' tit='Ресторан Зима' temp='$temp_p' wind='$wind_p' tr='$temp_r' wr='$wind_r' snow='$snow_p' img='$sky_p' />		  	
+			    <link url='http://172.17.6.97:8080/stream.flv' tit='Роза 1600' temp='$temp_r' wind='$wind_r' snow='$snow_r' img='$sky_r' />
+				<link url='http://172.17.6.97:8080/stream.flv' tit='Плато' temp='$temp_p' wind='$wind_p' snow='$snow_p' img='$sky_p' />                			
+			    ";
+				break;
+				
+				case 'Rosa_1600':
+				$v22 = 'Роза 1600';
+				$sbody = "
+				<link url='http://172.17.6.97:8080/stream.flv' tit='Роза 1600' temp='$temp_p' wind='$wind_p' tr='$temp_r' wr='$wind_r' snow='$snow_r' img='$sky_r' />
+				<link url='http://172.17.6.97:8080/stream.flv' tit='Плато' temp='$temp_p' wind='$wind_p' snow='$snow_p' img='$sky_p' />              
+			    <link url='http://172.17.6.97:8080/stream.flv' tit='Ресторан Зима' temp='$temp_p' wind='$wind_p' snow='$snow_p' img='$sky_p' />                			   	
+			    ";
+				break;
+				
+				case 'Post_2100':
+				$v22 = 'Лавинный пост';
+				$sbody = "
+				<link url='http://172.17.6.97:8080/stream.flv' tit='Лавинный пост' temp='$temp_p' wind='$wind_p' tr='$temp_r' wr='$wind_r' snow='$snow_r' img='$sky_r' />
+				<link url='http://172.17.6.97:8080/stream.flv' tit='Плато' temp='$temp_p' wind='$wind_p' snow='$snow_p' img='$sky_p' />              
+			    <link url='http://172.17.6.97:8080/stream.flv' tit='Ресторан Зима' temp='$temp_p' wind='$wind_p' snow='$snow_p' img='$sky_p' />                			   	
+			    ";
+				break;
+				
+				default:
+			    $v22 = null;
+				$sbody = "";           
+				break;
+			}
+$body="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<videos>";
+$body .= $sbody;
+$body .="</videos>";
+$path="tfeed.xml";
+$filenum=fopen($path,"w");
+fwrite($filenum,$body);
+fclose($filenum);
+?>
---
+++ lumi.php
@@ -0,0 +1,115 @@
+﻿<?php
+ 
+ class lumi{	  
+ const url = 'http://www.skiplan.com/php/getXmlInter.php?country=russia&region=alpes&resort=ROSA%20KHUTOR';
+ 
+ private static function get_feed(){
+             $ch = curl_init();
+            curl_setopt( $ch, CURLOPT_URL, self::url);
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+			curl_setopt($ch, CURLOPT_HEADER, false); 
+            curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5 );
+			curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+            $data = curl_exec( $ch );
+            curl_close( $ch );
+			$xml = simplexml_load_string($data);
+            return $xml;
+			}     
+
+
+			
+public static function get_data(){
+
+//Get serilaze data to Plato data from lumiplan xml feed
+
+  $v1 = self::get_feed();
+  $temp_p = $v1->WEATHER->PARAMETERS->AREA[0]->C_TEMPERATURE;
+  $wind_p = $v1->WEATHER->PARAMETERS->AREA[0]->DIRECTION.' '.$v1->WEATHER->PARAMETERS->AREA[0]->K_WIND;
+  $snow_p = $v1->WEATHER->PARAMETERS->AREA[0]->I_ACCUMULATION.' /'.$v1->WEATHER->PARAMETERS->AREA[0]->C_ACCUMULATION;
+  $img = $v1->WEATHER->PARAMETERS->AREA[0]->SKY_ID;
+  
+  ////Get serilaze data to Rosa 1600 data from lumiplan xml feed
+  
+  $temp_r = $v1->WEATHER->PARAMETERS->AREA[1]->C_TEMPERATURE;
+  $wind_r = $v1->WEATHER->PARAMETERS->AREA[1]->DIRECTION.' '.$v1->WEATHER->PARAMETERS->AREA[1]->K_WIND;
+  $snow_r = $v1->WEATHER->PARAMETERS->AREA[1]->I_ACCUMULATION.' /'.$v1->WEATHER->PARAMETERS->AREA[1]->C_ACCUMULATION;
+  $img1 = $v1->WEATHER->PARAMETERS->AREA[1]->SKY_ID;
+     
+  switch($img)
+			{
+				case 1:				
+				$img_fl1 = 'Sun';
+				break;
+				
+				case 2:				
+				$img_fl1 = 'Clody_Sun';
+				break;
+				
+				case 3:				
+				$img_fl1 = 'Clouds';
+				break;
+				
+				case 4:				
+				$img_fl1 = 'Cloud_Rain';
+				break;
+				
+				case 5:				
+				$img_fl1 = 'Cloud_Snow';
+				break;
+				
+				case 6:				
+				$img_fl1 = 'Cloud_Storm';
+				break;
+				
+				default:				
+				$img_fl1 = '';
+			}
+			
+	switch($img1)
+			{
+				case 1:				
+				$img_fl2 = 'Sun';
+				break;
+				
+				case 2:
+				$img_fl2 = 'Clody_Sun';
+				break;
+				
+				case 3:				
+				$img_fl2 = 'Clouds';
+				break;	
+				
+				case 4:				
+				$img_fl2 = 'Cloud_Rain';
+				break;
+				
+				case 5:				
+				$img_fl2 = 'Cloud_Snow';
+				break;
+				
+				case 6:				
+				$img_fl2 = 'Cloud_Storm';
+				break;
+				
+				default:				
+				$img_fl2 = '';
+			}
+			
+    $feed = array (
+	'temp_p' => $temp_p,
+	'wind_p' => $wind_p,
+	'snow_p' => $snow_p,
+	'sky_p'=> $img_fl1,
+	'temp_r' => $temp_r,
+	'wind_r' => $wind_r,
+	'snow_r' => $snow_r,
+	'sky_r'=> $img_fl2
+	);
+	 	
+   	      return $feed;     
+
+      }
+   
+ 
+   }
+?> 
